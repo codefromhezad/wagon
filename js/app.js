3113717e6e6ce8d54dcf6944a21831a7acf6c5b5
Pierrick <pierrick.varin@zagett.com>
2016-09-23 23:57:38 +0200

    Added assets + base CSS paralax for background landscape

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
+
+/* Timed animation loop maker */
+function loop(callback, max_progress) {
+	var start = null;
+
+	function step(timestamp) {
+		if (!start) start = timestamp;
+	 	var progress = timestamp - start;
+	 	
+	 	callback(progress);
+		
+		if( max_progress && progress >= max_progress ) {
+			return;
+		}
+		
+		window.requestAnimationFrame(step);
+	}
+
+	window.requestAnimationFrame(step);
+}
+
 jQuery( function($) {
+	
+	/* Caching common selectors */
+	var $landscape_wrapper = $('.wagon-container .landscape-wrapper');
+
+	/* Main animation loop */
+	/*loop(function(progress) {
+		$landscape_wrapper.css('transform', 'translateX(-'+(progress * 0.1)+'px)');
+	}, 2000);*/
 	
 });
